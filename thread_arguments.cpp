c25c119ffe997320d306500d8484b68a5b4bb4d9
Kevin W Matthews <KevinWMatthews@gmail.com>
2019-05-27 19:36:40 -0700

    Create function that prints to console

--- thread_arguments.cpp
+++ thread_arguments.cpp
@@ -1,4 +1,15 @@
+// Spawn a thread with an argument
+
+#include <thread>
+#include <iostream>
+
+void function(int param)
+{
+    std::cout << __func__ << " was passed: " << param << std::endl;
+}
+
 int main(void)
 {
+    function(42);
     return 0;
 }

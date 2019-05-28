1bfb50397bda29e19dc2e0341317c47fdcb85582
Kevin W Matthews <KevinWMatthews@gmail.com>
2019-05-27 18:57:12 -0700

    Update basic thread example

--- threads.cpp
+++ threads.cpp
@@ -1,6 +1,4 @@
 // Basic example of using threads.
-// This code is lacking a mutex; there is a data race on std::cout.
-// Fortunately, the standard library protects us from a crash.
 
 #include <thread>
 #include <iostream>
@@ -10,12 +8,13 @@ void function()
     std::cout << "In function\n";
 }
 
-
 int main(void)
 {
     std::cout << "Entering main\n";
+
     std::thread thread_handle { function };
     thread_handle.join();
+
     std::cout << "Exiting main\n";
     return 0;
 }

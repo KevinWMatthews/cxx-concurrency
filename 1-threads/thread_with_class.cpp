1dfe2dfb7a5998d736f99e2192286b7d771b5fdb
Kevin W Matthews <KevinWMatthews@gmail.com>
2019-06-13 10:54:45 -0700

    Add example of spawning a thread with a callable class

---
+++ 1-threads/thread_with_class.cpp
@@ -0,0 +1,27 @@
+// Spawn a thread and pass an instance of a class
+
+#include <thread>
+#include <iostream>
+
+class CallableObject
+{
+public:
+    void operator()();
+};
+
+void CallableObject::operator()()
+{
+    std::cout << "In class operator()\n";
+}
+
+int main(void)
+{
+    //TODO how to initialize this?
+    auto callable_object = CallableObject();
+    // CallableObject callable_object {};
+
+    std::thread a_thread { callable_object };
+    a_thread.join();
+
+    return 0;
+}

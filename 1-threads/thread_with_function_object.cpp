a00c394333738abb11a3cac8704f459f1a719fa9
Kevin W Matthews <KevinWMatthews@gmail.com>
2019-06-13 10:47:32 -0700

    Add example of passing a function object to a thread

---
+++ 1-threads/thread_with_function_object.cpp
@@ -0,0 +1,22 @@
+// Spawn a thread and pass it a function object
+
+#include <thread>
+#include <iostream>
+
+struct FunctionObject
+{
+    void operator()();
+};
+
+void FunctionObject::operator()()
+{
+    std::cout << "In function object\n";
+}
+
+int main(void)
+{
+    std::thread a_thread { FunctionObject() };
+    a_thread.join();
+
+    return 0;
+}

b751ce934f2cf2444fefdda7515b9d5df4ad5b5a
Kevin W Matthews <KevinWMatthews@gmail.com>
2019-06-16 22:27:58 -0700

    Update scoped locks example

--- scoped_locks.cpp
+++ scoped_locks.cpp
@@ -3,26 +3,34 @@
 #include <thread>
 #include <mutex>
 #include <iostream>
+#include <chrono>
 
 std::mutex shared_data_mutex;
 int shared_data;
 
+// Put the lock in its own scope to release the mutex as quickly as possible
 void increment(int value)
 {
     // Mutex is locked by the scoped lock's constructor
     std::scoped_lock lock {shared_data_mutex};
     shared_data += value;
+    std::cout << "Shared data: " << shared_data << std::endl;
 }   // Mutex is unlocked by the scoped lock's destructor
 
 void small_increment_task()
 {
-    // Put the lock in its own scope to release the mutex as quickly as possible
-    increment(1);
+    while (true) {
+        increment(1);
+        std::this_thread::sleep_for(std::chrono::milliseconds {500});
+    }
 }
 
 void large_increment_task()
 {
-    increment(100);
+    while (true) {
+        increment(100);
+        std::this_thread::sleep_for(std::chrono::milliseconds {1000});
+    }
 }
 
 int main(void)

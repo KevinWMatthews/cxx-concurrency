cc8ea303027fa8fb18362eb2c43ab31ca8037d5e
Kevin W Matthews <KevinWMatthews@gmail.com>
2019-06-16 22:20:09 -0700

    Tweak mutexes example

--- mutexes.cpp
+++ mutexes.cpp
@@ -4,22 +4,27 @@
 #include <thread>
 #include <mutex>
 #include <iostream>
+#include <thread>
+#include <chrono>
 
 std::mutex shared_data_mutex;
 int shared_data;
 
 void function()
 {
-    // If the mutex is unavailable, this blocks forever! This is a deadlock.
-    shared_data_mutex.lock();
-    shared_data += 1;
-    shared_data_mutex.unlock();
+    while (true) {
+        // If the mutex is unavailable, this blocks forever! This is a deadlock.
+        shared_data_mutex.lock();
+        shared_data += 1;
+        std::cout << "Shared data: " << shared_data << std::endl;
+        shared_data_mutex.unlock();
+        std::this_thread::sleep_for(std::chrono::milliseconds{500});
+    }
 }
 
 int main(void)
 {
     std::cout << __func__ << ": Entering" << std::endl;
-    std::cout << "Shared data: " << shared_data << std::endl;
 
     std::thread thread_1 {function};
     std::thread thread_2 {function};
@@ -27,6 +32,5 @@ int main(void)
     thread_2.join();
 
     std::cout << __func__ << ": Exiting" << std::endl;
-    std::cout << "Shared data: " << shared_data << std::endl;
     return 0;
 }

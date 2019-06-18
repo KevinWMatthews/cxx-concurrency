4ba681f83b941f3450adb5487cb64826380da0a1
Kevin W Matthews <KevinWMatthews@gmail.com>
2019-06-18 10:17:03 -0700

    Update future/promise example to match blog post
    Use the same verbiage and program structure.

--- 2-futures-promises/futures_and_promises.cpp
+++ 2-futures-promises/futures_and_promises.cpp
@@ -2,30 +2,20 @@
 #include <future>
 #include <iostream>
 
-void getter_task(std::future<int>& a_future)
-{
-    try
-    {
-        int value = a_future.get();
-        std::cout << "Got value: " << value << std::endl;
-    }
-    catch (...)
-    {
-        std::cout << "Future caught an exception!" << std::endl;
-    }
-}
-
-int calculate_result()
+int do_actual_work()
 {
     // throw "Make the promise fail";
     return 42;
 }
 
-void doer_task(std::promise<int>& a_promise)
+void worker_task(std::promise<int>& a_promise)
 {
     try
     {
-        int result = calculate_result();
+        std::cout << "Promise doing work." << std::endl;
+        int result = do_actual_work();
+
+        std::cout << "Promise setting value " << result << std::endl;
         a_promise.set_value(result);
     }
     catch (...)
@@ -37,19 +27,36 @@ void doer_task(std::promise<int>& a_promise)
     }
 }
 
-int main(void)
+void parent_task()
 {
     // Create a promise/future pair
     std::promise<int> the_promise;
-    std::future<int> the_future  { the_promise.get_future() };
+    std::future<int> the_future { the_promise.get_future() };
 
     // Order doesn't matter - the future waits for a value from the promise, and
     // the promise waits for its future to receive the value.
-    std::thread getter_thread { getter_task, std::ref(the_future) };
-    std::thread doer_thread { doer_task, std::ref(the_promise) };
+    std::thread worker_thread { worker_task, std::ref(the_promise) };
+    worker_thread.detach();
+    // Go do cool stuff
 
-    getter_thread.join();
-    doer_thread.join();
+    int value;
+    try
+    {
+        value = the_future.get();
+    }
+    catch (...)
+    {
+        std::cout << "Future caught an exception!" << std::endl;
+        return;
+    }
+
+    std::cout << "Future got value: " << value << std::endl;
+}
+
+int main(void)
+{
+    std::thread parent_thread {parent_task};
+    parent_thread.join();
 
     return 0;
 }

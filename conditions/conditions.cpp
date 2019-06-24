d8429c1cb289bdafb137c688fa11acb314eea4d9
Kevin W Matthews <KevinWMatthews@gmail.com>
2019-06-24 10:52:05 -0700

    Add simplified example of sharing data using a condition variable

--- conditions/conditions.cpp
+++ conditions/conditions.cpp
@@ -1,11 +1,24 @@
 #include <thread>
+#include <mutex>
+#include <condition_variable>
 #include <chrono>
+#include <iostream>
+
+static std::condition_variable data_condition;
+static std::mutex data_mutex;
+int data;
+bool data_ready;
 
 void producer_task()
 {
     while (true)
     {
+        std::cout << __func__ << ": Setting data\n";
         std::this_thread::sleep_for(std::chrono::seconds(1));
+        std::scoped_lock lock {data_mutex};
+        data ^= data;
+        data_ready = true;
+        data_condition.notify_one();
     }
 }
 
@@ -13,7 +26,13 @@ void consumer_task()
 {
     while (true)
     {
-        ;
+        std::cout << __func__ << ": Waiting\n";
+        std::unique_lock lock {data_mutex};
+        data_condition.wait(lock, [](){ return data_ready; });
+        int local_copy = data;
+        data_ready = false;
+        lock.unlock();
+        std::cout << __func__ << ": Got value: " << local_copy << "\n";
     }
 }
 

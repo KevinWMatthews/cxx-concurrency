95c814fb84e018482103b72942fa0a5ee72ba409
Kevin W Matthews <KevinWMatthews@gmail.com>
2019-06-24 10:40:49 -0700

    Create empty producer thread

--- conditions/conditions.cpp
+++ conditions/conditions.cpp
@@ -1,4 +1,17 @@
+#include <thread>
+#include <chrono>
+
+void producer_task()
+{
+    while (true)
+    {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+}
+
 int main(void)
 {
+    std::thread producer {producer_task};
+    producer.join();
     return 0;
 }
